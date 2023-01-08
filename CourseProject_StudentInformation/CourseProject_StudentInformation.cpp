5bf1a33dce5eb63784dfff42f7fccee87ac7f7f8
KristinaKalemdzhieva <krisi100203@gmail.com>
2023-01-08 14:31:33 +0200

    Creating the main menu and it's logic.

--- CourseProject_StudentInformation/CourseProject_StudentInformation.cpp
+++ CourseProject_StudentInformation/CourseProject_StudentInformation.cpp
@@ -1,6 +1,70 @@
 ﻿#include <iostream>
 
+void groupMenu(const int group)
+{
+
+}
+
+void printStudentsFromVariousGroups()
+{
+
+}
+
+void mainMenu()
+{
+	std::cout << "1. First Group Menu" << std::endl;
+	std::cout << "2. Second Group Menu" << std::endl;
+	std::cout << "3. Third Group Menu" << std::endl;
+	std::cout << "4. Fourth Group Menu" << std::endl;
+	std::cout << "5. Fifth Group Menu" << std::endl;
+	std::cout << "6. Sixth Group Menu" << std::endl;
+	std::cout << "7. Seventh Group Menu" << std::endl;
+	std::cout << "8. Eigth Group Menu" << std::endl;
+	std::cout << "9. Print student information for more than one group" << std::endl;
+	std::cout << "10. Exit" << std::endl << std::endl;
+
+	std::cout << "Choose a number: ";
+	int n;
+	std::cin >> n;
+
+	switch (n)
+	{
+	case 1:
+		groupMenu(1);
+		break;
+	case 2:
+		groupMenu(2);
+		break;
+	case 3:
+		groupMenu(3);
+		break;
+	case 4:
+		groupMenu(4);
+		break;
+	case 5:
+		groupMenu(5);
+		break;
+	case 6:
+		groupMenu(6);
+		break;
+	case 7:
+		groupMenu(7);
+		break;
+	case 8:
+		groupMenu(8);
+		break;
+	case 9:
+		printStudentsFromVariousGroups();
+		break;
+	case 10:
+		return;
+		break;
+	}
+}
+
+
+
 int main()
 {
-    std::cout << "Hello world";
+	mainMenu();
 }
